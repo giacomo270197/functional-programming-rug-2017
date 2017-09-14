480f7bac9802380dfdf47d997fb7b505e70b9bb0
giacomo270197 <gcasoni@hotmail.it>
2017-09-14 10:57:17 +0200

    homework

--- remainder.hs
+++ remainder.hs
@@ -1,10 +1,14 @@
+{-super :: [Double] -> [Double] -> ([Double], [Double])
+super xs ys = -}
+
+
 remainder :: [Double] -> [Double] -> [Double]
 remainder xs ys
        | length xs < length ys         = xs
        | otherwise                     = remainder (step xs ys) ys
 
 step :: [Double] -> [Double] -> [Double]
-step (x:xs) (y:ys) = clean sub (x:xs) (map (* (round x `div` round y)) (y:ys))
+step (x:xs) (y:ys) = clean (sub (x:xs) (map (* ((/) x y)) (y:ys)))
 
 sub :: [Double] -> [Double] -> [Double]
 sub xs ys = [x-y | (x,y) <- listGen xs ys]
@@ -17,5 +21,5 @@ fill xs n = xs ++ replicate n 0
  
 clean :: [Double] -> [Double]
 clean (x:xs)
-     |  x /= 0      = xs
-     |  otherwise   = clean xs
+     |  x /= 0            = (x:xs)
+     |  otherwise         = clean xs
